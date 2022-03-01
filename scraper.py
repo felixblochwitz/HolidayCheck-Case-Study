2dbcff8abc38de0330a04d6c0340cd8299bbd97f
felixblochwitz <44145511+felixblochwitz@users.noreply.github.com>
2022-03-01 16:19:55 +0100

    test

--- scraper.py
+++ scraper.py
@@ -3,19 +3,23 @@ import json
 
 
 # building api request string
-start_date = "" #in format yyyy-mm-dd
 api_key = "91f67b2a-520b-4f9f-a4ca-edcb0d610454"
-query = 
-#NOTE: api documentation: https://open-platform.theguardian.com/documentation/
-api_str = f'https://content.guardianapis.com/search?q="{query}"&from-date={start_date}&api-key={api_key}'
-
-
+# NOTE: api documentation: https://open-platform.theguardian.com/documentation/
 
 
 # how to pretty print json
-result = req.get(test_str).content
-parsed = json.loads(result)
-print(json.dumps(parsed, indent=4, sort_keys=True))
+# result = req.get(test_str).content
+# parsed = json.loads(result)
+# print(json.dumps(parsed, indent=4, sort_keys=True))
 
 
+def api_wrapper(query, start_date, api_key):
+    api_str = f'https://content.guardianapis.com/search?q="{query}"&from-date={start_date}&api-key={api_key}'
 
+    result_json = req.get(api_str).content
+    return result_json
+
+
+my_json = api_wrapper("Trudeau", "2022-02-22", api_key)
+
+print(my_json)
